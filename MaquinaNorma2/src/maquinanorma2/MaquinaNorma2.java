e7a23dc098c72e954151131fe0a593eeadd98c79
04557105114 <04557105114@PSO-CGTI04.ifto.local>
2017-05-19 14:21:38 -0300

    Test Git

---
+++ MaquinaNorma2/src/maquinanorma2/MaquinaNorma2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquinanorma2;
+
+/**
+ *
+ * @author 04557105114
+ */
+public class MaquinaNorma2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
